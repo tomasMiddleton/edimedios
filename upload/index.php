2a7b4074099353b244a5e7a8edf4bb974806f453
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-08 12:56:38 -0300

    asd

--- upload/index.php
+++ upload/index.php
@@ -1,4 +1,5 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
