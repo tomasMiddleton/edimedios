94cbb4ba2694cd776a8948b36c439664774cb394
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-08 12:26:51 -0300

    v1

--- upload/index.php
+++ upload/index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo "Subiendo";
+    
+    
+    //datos del arhivo
+    $nombre_archivo = $_FILES['file']['name'];
+    $tipo_archivo = $_FILES['file']['type'];
+    $tamano_archivo = $_FILES['file']['size'];
+        
+    //compruebo si las características del archivo son las que deseo
+    if (!((strpos($tipo_archivo, "gif") || strpos($tipo_archivo, "jpeg")) && ($tamano_archivo < 100000))) {
+        echo "La extensión o el tamaño de los archivos no es correcta. <br><br><table><tr><td><li>Se permiten archivos .gif o .jpg<br><li>se permiten archivos de 100 Kb máximo.</td></tr></table>";
+    }else{
+        if (move_uploaded_file($_FILES['file']['tmp_name'],  $nombre_archivo)){
+                echo "El archivo ha sido cargado correctamente.";
+        }else{
+                echo "Ocurrió algún error al subir el fichero. No pudo guardarse.";
+        }
+    }
+    echo "Uploaded";
 ?>
