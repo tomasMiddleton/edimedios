0e356d93fe391ecc3bad1371a51cccd8ba536347
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-09 12:08:00 -0300

    as

--- upload/index.php
+++ upload/index.php
@@ -6,7 +6,7 @@
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
     header('content-type: application/json; charset=utf-8');
     
-    
+    /*
     //datos del arhivo
     $nombre_archivo = $_FILES['file']['name'];
     $tipo_archivo = $_FILES['file']['type'];
@@ -23,4 +23,22 @@
         }
     }
     echo "Uploaded";
+    */
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
+
 ?>
