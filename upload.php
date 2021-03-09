18d9c1c323c6c285b8d8c0fc2bef6d799cce3c9b
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-09 19:24:19 -0300

    asdf

---
+++ upload.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+header('content-type: application/json; charset=utf-8');
+
+echo "Uploaded";
+ 
