955ff73fa67d9aa3133b790bcbd1305836dcebed
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-09 19:45:40 -0300

    asd

--- upload.php
+++ upload.php
@@ -5,5 +5,20 @@ header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 header('content-type: application/json; charset=utf-8');
 
+
+$dir_subida = './uploads/';
+$fichero_subido = $dir_subida . basename($_FILES['filepond']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['filepond']['tmp_name'], $fichero_subido)) {
+    echo "El fichero es válido y se subió con éxito.\n";
+} else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+}
+
+echo 'Más información de depuración:';
+print_r($_FILES);
+
+print "</pre>";
 echo "Uploaded";
  
