2a51eb617536bf052bb29d355251183c3ac9f4e9
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2021-03-09 23:45:20 -0300

    asd

--- get_token.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Credentials: true");
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-header('content-type: application/json; charset=utf-8');
-
-echo "Uploaded";
- 
